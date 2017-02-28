90edfa79cfbd993cec41ff870ada626adb8e4993
Leah Smith <leahsmith@csu.fullerton.edu>
2017-02-28 08:39:43 -0800

    Restored main.cpp
    un-commented out the main function, took out our debugging code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <cassert>
-#include <algorithm>
+
 
 #include "MedicalRecord.h"
 #include "Baby.h"
@@ -12,59 +12,58 @@ using namespace std;
 
 int main() {
 	try {
-//		{
+		{
 			/* test only the Baby class*/
-			//Baby babyTest("Testname", 1000);
-			//assert(babyTest.getName() == "Testname");
-			//assert(babyTest.getWeight() == 1000);
-	/*Baby j("Jazmin", 50);
-	cout << babyTest.getName();*/
+			Baby babyTest("Testname", 1000);
+			assert(babyTest.getName() == "Testname");
+			assert(babyTest.getWeight() == 1000);
 
 
 
 
-			//}
 
-			//{   // test full code with a small data file
-			//	MedicalRecord MR;
-			//	MR.buildMedicalRecordfromDatafile("baby_data_small.txt"); // build a medical record from the file of baby names and weights\
+		}
 
-			//	int nBirths = MR.numberOfBirths();
-			//	cout << "Number of births: " << nBirths << endl;
-			//	assert(nBirths == 10);
+			{   test full code with a small data file
+			MedicalRecord MR;
+			MR.buildMedicalRecordfromDatafile("baby_data_small.txt"); // build a medical record from the file of baby names and weights\
 
-			//	int nEmma = MR.numberOfBabiesWithName("Emma");
-			//	cout << "Number of babies with name Emma: " << nEmma << endl;
-			//	assert(nEmma == 2);
+			int nBirths = MR.numberOfBirths();
+			cout << "Number of births: " << nBirths << endl;
+			assert(nBirths == 10);
 
-			//		int nLow = MR.numberOfBabiesWithLowBirthWeight();
-			//		cout << "Number of babies with low birth weight: " << nLow << endl;
-			//		assert(nLow == 2);
+			int nEmma = MR.numberOfBabiesWithName("Emma");
+			cout << "Number of babies with name Emma: " << nEmma << endl;
+			assert(nEmma == 2);
 
-			//		string mostPopularName = MR.mostPopularName();
-			//		cout << "Most popular baby name: " << mostPopularName << endl;
-			//		assert (mostPopularName == "Sophia");
-			//	}
+			int nLow = MR.numberOfBabiesWithLowBirthWeight();
+			cout << "Number of babies with low birth weight: " << nLow << endl;
+			assert(nLow == 2);
+
+			string mostPopularName = MR.mostPopularName();
+			cout << "Most popular baby name: " << mostPopularName << endl;
+			assert (mostPopularName == "Sophia");
+			}
 		
-			{   // test full code with a large data file
-					MedicalRecord MR;
-					MR.buildMedicalRecordfromDatafile("baby_data_large.txt"); // build a medical record from the file of baby names and weights\
+			{   test full code with a large data file
+			MedicalRecord MR;
+			MR.buildMedicalRecordfromDatafile("baby_data_large.txt"); // build a medical record from the file of baby names and weights\
 		
-					int nBirths = MR.numberOfBirths();
-					cout << "Number of births: " << nBirths << endl;
-					assert (nBirths == 199604);
+			int nBirths = MR.numberOfBirths();
+			cout << "Number of births: " << nBirths << endl;
+			assert (nBirths == 199604);
 		
-					int nEva = MR.numberOfBabiesWithName("Eva");
-					cout << "Number of babies with name Eva: " << nEva << endl;
-					assert (nEva == 566);
+			int nEva = MR.numberOfBabiesWithName("Eva");
+			cout << "Number of babies with name Eva: " << nEva << endl;
+			assert (nEva == 566);
 		
-					int nLow = MR.numberOfBabiesWithLowBirthWeight();
-					cout << "Number of babies with low birth weight: " << nLow << endl;
-					assert (nLow == 15980);
+			int nLow = MR.numberOfBabiesWithLowBirthWeight();
+			cout << "Number of babies with low birth weight: " << nLow << endl;
+			assert (nLow == 15980);
 		
-					string mostPopularName = MR.mostPopularName();
-					cout << "Most popular baby name: " << mostPopularName << endl;
-					assert (mostPopularName == "Emma");
+			string mostPopularName = MR.mostPopularName();
+			cout << "Most popular baby name: " << mostPopularName << endl;
+			assert (mostPopularName == "Emma");
 			}
 		}
 	catch (exception &e) {
